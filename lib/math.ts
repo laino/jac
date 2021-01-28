0a46fa7b7bc99bfd619c9df7340a00268575f595
Nils Kuhnhenn <lain@volafile.org>
2021-01-28 16:02:08 +0100

    ....

--- lib/math.ts
+++ lib/math.ts
@@ -35,6 +35,7 @@ export interface PointWeight {
 }
 
 export interface CloudSettings {
+    dimensions: number,
     maxPoints: number
 }
 
@@ -55,25 +56,52 @@ function Point(v: number, di: number[]) {
 export class Dimension {
     public points: Point[] = [];
 
+    public min = 0;
+    public max = 0;
+
     public constructor(public n: number) {
     }
     
-    public add(point: Point) {
-        this.points.push(point);
+    public add(P: Point) {
+        this.points.push(P);
     }
     
     public update() {
+        const points = this.points;
         const n = this.n;
 
-        this.points.sort((a, b) => {
-            const dDiff = a.d[n] - b.d[n];
+        points.sort((a, b) => {
+            const aD = a.d;
+            const bD = b.d;
+
+            const dDiff = aD[n] - bD[n];
 
             if (dDiff !== 0) {
                 return dDiff;
             }
 
-            return a.v - b.v;
+            for (let i = 0; i < aD.length; i++) {
+                if (i === n) {
+                    continue;
+                }
+
+                const dDiff = aD[i] - bD[i];
+
+                if (dDiff !== 0) {
+                    return dDiff;
+                }
+            }
+
+            return 0;
         });
+
+        if (points.length) {
+            this.min = points[0].d[n];
+            this.max = points[points.length - 1].d[n];
+        } else {
+            this.min = 0;
+            this.max = 0;
+        }
     }
     
     public remove(p: Point): boolean {
@@ -98,16 +126,16 @@ export class Dimension {
     public range(x: number, y: number): PointWeight[] {
         const points = this.points;
 
-        if (x - y === 0 || points.length < 2) {
+        if (x - y === 0 || points.length < 1) {
             return [];
         }
 
         const weights: PointWeight[] = [];
 
         let min = this.indexLT(x);
-
         let max = min + 1;
 
+        // Add fully enclosed points and calculated max
         for (; max < points.length; max++) {
             const C = points[max];
             const Cv = C.d[this.n];
@@ -120,64 +148,82 @@ export class Dimension {
             weights.push(PointWeight(C, 1));
         }
             
-        // Weights is now the list of fully enclosed points
-
+        // We didn't enclose any point
         if (weights.length === 0) {
-            // range encloses no points
-            // TODO
+            const ratioA = this.interpolate(min, x);
+            const ratioB = this.interpolate(min, y);
+
+            if (min === -1) {
+                weights.push(PointWeight(points[0], ratioA - ratioB));
+            } else if (max === points.length) {
+                weights.push(PointWeight(points[points.length - 1], ratioB - ratioA));
+            }
+
+            if (ratioA < 0.5) {
+                if (ratioB < 0.5) {
+                    weights.push(PointWeight(points[min], ratioB - ratioA));
+                } else {
+                    weights.push(PointWeight(points[min], 0.5 - ratioA));
+                }
+            }
+            
+            if (ratioB > 0.5) {
+                if (ratioA > 0.5) {
+                    weights.push(PointWeight(points[min], ratioB - ratioA));
+                } else {
+                    weights.push(PointWeight(points[min], ratioB - 0.5));
+                }
+            }
+
             return weights;
         }
 
         // Interpolate between first point and the outside
-        if (min > -1) {
-            const ratio = this.interpolate(min, x);
+        let ratio = this.interpolate(min, x);
 
-            if (ratio >= 0.5) {
-                let i = 0;
-                const cmp = weights[i].P.d[this.n];
-                for (; i < weights.length && cmp === weights[i].P.d[this.n]; i++) {
-                    weights[i].w += 0.5 - ratio;
-                }
-            } else {
-                let i = min;
-                const cmp = points[i].d[this.n];
-                for (; i >= 0 && cmp === points[i].d[this.n]; i--) {
-                    weights.push(PointWeight(points[i], 0.5 - ratio));
-                }
+        if (ratio >= 0.5) {
+            let i = 0;
+            const cmp = weights[i].P.d[this.n];
+            for (; i < weights.length && cmp === weights[i].P.d[this.n]; i++) {
+                weights[i].w += 0.5 - ratio;
+            }
+        } else if (min > -1) {
+            let i = min;
+            const cmp = points[i].d[this.n];
+            for (; i >= 0 && cmp === points[i].d[this.n]; i--) {
+                weights.push(PointWeight(points[i], 0.5 - ratio));
             }
         }
         
         // Interpolate between last point and the outside
-        if (max < points.length) {
-            const ratio = this.interpolate(max - 1, y);
+        ratio = this.interpolate(max - 1, y);
 
-            if (ratio > 0.5) {
-                let i = max;
-                const cmp = points[i].d[this.n];
-                for (; i < points.length && cmp === points[i].d[this.n]; i++) {
-                    weights.push(PointWeight(points[i], ratio - 0.5));
-                }
-            } else {
-                let i = weights.length - 1;
-                const cmp = weights[i].P.d[this.n];
-                for (; i >= 0 && cmp === weights[i].P.d[this.n]; i--) {
-                    weights[i].w -= 0.5 - ratio;
-                }
+        if (ratio <= 0.5) {
+            let i = weights.length - 1;
+            const cmp = weights[i].P.d[this.n];
+            for (; i >= 0 && cmp === weights[i].P.d[this.n]; i--) {
+                weights[i].w -= 0.5 - ratio;
+            }
+        } else if (max < points.length) {
+            let i = max;
+            const cmp = points[i].d[this.n];
+            for (; i < points.length && cmp === points[i].d[this.n]; i++) {
+                weights.push(PointWeight(points[i], ratio - 0.5));
             }
         }
-            
+
         return weights;
     }
 
-    private interpolate(a: number, x: number) {
+    private interpolate(a: number, x: number): number {
         const points = this.points;
         
-        if (a + 1 == points.length) {
-            return -this.interpolate(a - 1, x);
-        }
-        
         if (a === -1) {
-            return -this.interpolate(a + 1, x);
+            return 1 + this.interpolate(a + 1, x);
+        }
+
+        if (a + 1 == points.length) {
+            return this.interpolate(a - 1, x) - 1;
         }
 
         const A = points[a];
@@ -189,20 +235,47 @@ export class Dimension {
         // Using different functions yields different range selection behaviors
         return (x - A.d[this.n]) / d;
     }
+    
+    public getAt(x: number): Point[] {
+        const points = this.points;
+        const results: Point[] = [];
+
+        let i = this.indexLT(x) + 1;
+
+        for (; i < points.length; i++) {
+            const P = points[i];
+
+            if (P.d[this.n] !== x) {
+                break;
+            }
+
+            results.push(P);
+        }
+
+        return results;
+    }
 
     public indexLT(x: number): number {
         const points = this.points;
 
-        let min = 0;
-        let max = points.length;
+        if (x <= this.min) {
+            return -1;
+        }
 
-        while (min < max) {
+        if (x > this.max) {
+            return points.length - 1;
+        }
+
+        let min = 0;
+        let max = points.length - 1;
+
+        while (min <= max) {
             const i = Math.floor((min + max) / 2);
             const v = points[i].d[this.n];
 
-            if (v > x) {
+            if (x < v) {
                 max = i - 1;
-            } else if (v < x && (i + 1 === points.length || points[i + 1].d[this.n] >= x)) {
+            } else if (x > v && (i + 1 === points.length || x <= points[i + 1].d[this.n])) {
                 return i;
             } else {
                 min = i + 1;
@@ -216,27 +289,34 @@ export class Dimension {
 export class Cloud {
     private points: Point[] = [];
     private dims: Dimension[] = [];
-    private needsUpdate = false;
+    private dimensions: number;
 
-    public constructor(private dimensions: number, private settings: CloudSettings) {
-        for (let i = 0; i < dimensions; i++) {
+    public needsUpdate = false;
+
+    public displacement: number;
+    public readonly settings: CloudSettings;
+
+    public constructor(settings: CloudSettings) {
+        this.dimensions = settings.dimensions;
+
+        this.settings = Object.freeze(Object.assign({}, settings));
+
+        for (let i = 0; i < this.dimensions; i++) {
             this.dims.push(new Dimension(i));
         }
     }
 
     public getPoints(): Point[] {
-        this.update();
-
         return this.points;
     }
 
     public add(volume: number, d: number[]) {
-        this.needsUpdate = true;
-
         if (volume <= 0) {
             return;
         }
-
+        
+        this.needsUpdate = true;
+ 
         const P = Point(volume, d);
 
         this.points.push(P);
@@ -246,15 +326,8 @@ export class Cloud {
         }
     }
 
-    public range(from: number[], to: number[]): Point {
+    public range(from: number[], to: number[]): Selection { 
         this.update();
-
-        let Rd: number[];
-        let v: number = 0;
-        
-        Rd = []
-        Rd.length = this.dimensions;
-        Rd.fill(0);
 
         const selection = new Map<Point, number>();
 
@@ -271,152 +344,138 @@ export class Cloud {
             const seen = new Set<Point>();
 
             for (const {P, w} of range) {
+                if (!selection.has(P)) {
+                    continue;
+                }
+
+                const sw = selection.get(P) * w;
+
                 seen.add(P);
 
-                if (selection.has(P)) {
-                    selection.set(P, selection.get(P) * w);
-                }
+                selection.set(P, sw);
             }
 
             for (const P of selection.keys()) {
-                seen.delete(P);
-            }
-            
-            for (const P of seen) {
-                selection.delete(P);
+                if (!seen.has(P)) {
+                    selection.delete(P);
+                }
             }
         }
-
-        for (const [P, w] of selection.entries()) {
-            v += P.v * w;
-        }
-
-        for (const [P, w] of selection.entries()) {
-            for (let i = 0; i < this.dimensions; i++) {
-                Rd[i] += P.d[i] * P.v * w / v;
-            }
-        }
-
-        return Point(v, Rd);
+        
+        return new Selection(this, selection);
     }
 
-    public sample(d: number[]): Point {
+    public point(d: number[]): Selection {
         this.update();
 
         const points = this.points;
 
-        const {sum, weights} = this.weights(d);
+        const weights = this.weights(d);
 
-        const wInv = sum ? (1 / sum) : 1;
-        
-        let Rd: number[];
-        let v: number = 0;
-        
-        if (sum) {
-            Rd = []
-            Rd.length = this.dimensions;
-            Rd.fill(0);
-        } else {
-            Rd = d;
-        }
+        const selection = new Map<Point, number>();
 
         for (let i = 0; i < points.length; i++) {
             const P = points[i];
-            const w = weights[i] * wInv;
+            const w = weights[i];
 
-            v += P.v * w;
-
-            if (sum) {
-                for (let i2 = 0; i2 < this.dimensions; i2++) {
-                    Rd[i2] += P.d[i2] * w;
-                }
-            }
+            selection.set(P, w);
         }
         
-        const R = Point(v, Rd);
+        return new Selection(this, selection);
+    }
 
-        return R;
+    private removeDuplicates() {
+        const dPoints = this.dims[0].points;
+
+        let removedAny = false;
+
+        // Find duplicate points
+        outer: for (let i = dPoints.length - 1; i > 0; i--) {
+            const P = dPoints[i - 1];
+            const N = dPoints[i];
+
+            const Pd = P.d;
+            const Nd = N.d;
+
+            for (let i = 0; i < this.dimensions; i++) {
+                if (Pd[i] !== Nd[i]) {
+                    continue outer;
+                }
+            }
+
+            P.v += N.v;
+
+            this.removeAt(i);
+
+            removedAny = true;
+        }
+
+        return removedAny;
     }
     
     private simplify() {
         const points = this.points;
 
-        let displacement = 0;
+        if (points.length <= this.settings.maxPoints) {
+            return false;
+        }
 
-        outer: do {
-            for (let i = 0; i < points.length; i++) {
-                const P = points[i];
+        // TODO: with this.weights this is O(n^2)
+        for (let i = 0; i < points.length; i++) {
+            const P = points[i];
 
-                if (this.isBounds(P)) {
-                    continue;
-                }
-
-                const {sum, weights} = this.weights(P.d, P);
-
-                if (sum === 0) {
-                    for (let i2 = points.length - 1; i2 >= 0; i2--) {
-                        if (weights[i2] !== 1) {
-                            continue;
-                        }
-
-                        const P2 = points[i2];
-
-                        P.v += P2.v;
-
-                        this.removeAt(i2);
-                    }
-                } else if (points.length > this.settings.maxPoints) {
-                    for (let i2 = points.length - 1; i2 >= 0; i2--) {
-                        const P2 = points[i2];
-                        const V = P.v / sum * weights[i2];
-                        const combinedV = P2.v + V;
-                        
-                        for (let i3 = 0; i3 < this.dimensions; i3++) {
-                            const old = P2.d[i3];
-
-                            P2.d[i3] = ((old * P2.v) + (P.d[i3] * V)) / combinedV;
-                        }
-                        
-                        P2.v = combinedV;
-                    }
-
-                    displacement += P.v;
-
-                    this.removeAt(i);
-                } else {
-                    continue;
-                }
-
-                continue outer;
+            if (this.isBounds(P)) {
+                continue;
             }
 
-            break;
-        } while (points.length > this.settings.maxPoints);
+            const weights = this.weights(P.d, P);
 
-        return {displacement};
+            for (let i2 = points.length - 1; i2 >= 0; i2--) {
+                const P2 = points[i2];
+                const V = P.v * weights[i2];
+
+                if (V === 0) {
+                    continue;
+                }
+
+                const combinedV = P2.v + V;
+                
+                for (let i3 = 0; i3 < this.dimensions; i3++) {
+                    const old = P2.d[i3];
+
+                    P2.d[i3] = ((old * P2.v) + (P.d[i3] * V)) / combinedV;
+                }
+                
+                P2.v = combinedV;
+            }
+
+            this.displacement += P.v;
+
+            this.removeAt(i);
+
+            return true;
+        }
+
+        return false;
     }
-    
     
     private update() {
         if (!this.needsUpdate) {
             return;
         }
 
-        this.simplify();
-
         this.needsUpdate = false;
 
-        for (const d of this.dims) {
-            d.update();
-        }
+        do  {
+            for (const d of this.dims) {
+                d.update();
+            }
 
-        this.points.sort((a, b) => a.v - b.v);
+            this.points.sort((a, b) => a.v - b.v);
+        } while (this.removeDuplicates() || this.simplify())
     }
 
-
-    public removeAt(i: number, n = 1) {
-        this.needsUpdate = true;
-
+    private removeAt(i: number, n = 1) {
         const P = this.points[i];
 
         this.points.splice(i, n);
@@ -438,20 +497,19 @@ export class Cloud {
         const points = this.points;
         const weights: number[] = [];
 
+        let zeros = 0;
         let sum = 0;
 
-        let distWasZero = false;
-
-        for (const p of this.points) {
-            if (p === exclude) {
+        for (const P of this.points) {
+            if (P === exclude) {
                 weights.push(0);
                 continue;
             }
 
-            const dist = _d(p.d, d, this.dimensions);
+            const dist = this.distance(P.d, d);
 
             if (dist === 0) {
-                distWasZero = true;
+                zeros++;
                 weights.push(0);
             } else {
                 const w = 1 / dist;
@@ -460,34 +518,76 @@ export class Cloud {
             }
         }
 
-        if (distWasZero) {
-            sum = 0;
+        if (zeros) {
+            const zerosInv = 1 / zeros;
 
             for (let i = 0; i < points.length; i++) {
                 if (weights[i] !== 0) {
                     weights[i] = 0;
                 } else if (points[i] !== exclude) {
-                    weights[i] = 1;
+                    weights[i] = zerosInv;
                 }
+            }
+        } else {
+            const sumInv = 1 / sum;
+            for (let i = 0; i < points.length; i++) {
+                weights[i] *= sumInv;
             }
         }
 
-        return {
-            sum, weights
-        };
-    }    
-}
-
-function _d(A: number[], B: number[], dimensions: number) {
-    let sum = 0;
-
-    for (let i = 0; i < dimensions; i++) {
-        sum += (B[i] - A[i]) * (B[i] - A[i]);
+        return weights;
     }
 
-    return sum;
+    private distance(a: number[], b: number[]) {
+        // Simplified because we only use this
+        // for relative comparisons anyways
+
+        let dist = 0;
+
+        for (let i = 0; i < this.dimensions; i++) {
+            const d = this.dims[i];
+            let rD = d.max - d.min;
+
+            if (rD === 0) {
+                continue;
+            }
+
+            const r = (a[i] - b[i]) / (d.max - d.min);
+
+            dist += r * r;
+        }
+
+        return dist;
+    }
 }
 
-export function distance(A: number[], B: number[], dimensions: number) {
-    return Math.sqrt(_d(A,B, dimensions));
+class Selection {
+    public constructor(
+        private cloud: Cloud,
+        private selection: Map<Point, number>) {
+    }
+
+    public sum() {
+        const dimensions = this.cloud.settings.dimensions;
+        const s = this.selection;
+
+        let Rd: number[];
+        let v: number = 0;
+        
+        Rd = [];
+        Rd.length = dimensions;
+        Rd.fill(0);
+
+        for (const [P, w] of s.entries()) {
+            v += P.v * w;
+        }
+
+        for (const [P, w] of s.entries()) {
+            for (let i = 0; i < dimensions; i++) {
+                Rd[i] += P.d[i] * P.v * w / v;
+            }
+        }
+
+        return Point(v, Rd);
+    }
 }
--- test/run.ts
+++ test/run.ts
@@ -1,20 +1,15 @@
 
 import * as math from 'math';
 
-const cloud = new math.Cloud(3, {maxPoints: 4});
+const cloud = new math.Cloud({
+    dimensions: 1,
+    maxPoints: 8
+});
 
-cloud.add(1, [1, 1, 1]);
-cloud.add(1, [2, 1, 1]);
-cloud.add(1, [1, 2, 1]);
-cloud.add(1, [2, 2, 1]);
+cloud.add(1, [1]);
+cloud.add(2, [1]);
+cloud.add(2, [2]);
 
-cloud.add(1, [0, 0, 1]);
-cloud.add(1, [0, 3, 1]);
-cloud.add(1, [3, 0, 1]);
-cloud.add(1, [3, 3, 1]);
-
-const range = cloud.range([-5, -5, 1], [5, 5, 1]);
-
-console.log(range.v * range.d[0], range.v * range.d[1], range.v);
+const range = cloud.range([0], [3]);
 
 console.log(cloud.getPoints());
